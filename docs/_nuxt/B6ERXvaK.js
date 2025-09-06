36d034c496d69a25983766e10f5fe5c364b48c63
VictorG <victorericksongv@gmail.com>
2025-09-06 11:48:27 -0400

    Delete docs directory

--- docs/_nuxt/B6ERXvaK.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./DlAUqK2U.js";import{c,o}from"./BzUmCo16.js";const r={};function t(n,_){return o(),c("main")}const f=e(r,[["render",t]]);export{f as default};
--- docs/_nuxt/CtFBWL5o.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./DlAUqK2U.js";import{c,o}from"./Bx61mS2s.js";const r={};function t(n,_){return o(),c("main")}const f=e(r,[["render",t]]);export{f as default};
